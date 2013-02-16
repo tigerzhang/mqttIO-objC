5749293911f0f2fcc147b3660313c4e5c34449bf
Sergey Loshchilov <sergei@2lemetry.com>
2013-02-16 11:46:35 +0400

    QoS 1 and 2 support. LWT parameters support for MQTT session.

--- dist/MqttSDK-Release-armv7s/include/MQTTSession.h
+++ dist/MqttSDK-Release-armv7s/include/MQTTSession.h
@@ -3,7 +3,7 @@
 //  MQtt Client
 //
 //  Created by Kyle Roche on 8/21/11.
-//  Copyright (c) 2011 2lemetry. All rights reserved.
+//  Copyright (c) 2011 2lemetry, LLC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -26,21 +26,25 @@ typedef enum {
 } MQTTSessionEvent;
 
 @interface MQTTSession : NSObject {
-    MQTTSessionStatus   status;
-    NSString*           clientId;
-    NSString*           userName;
-    NSString*           password;
-    UInt16              keepAliveInterval;
-    BOOL                cleanSessionFlag;
-    NSRunLoop*          runLoop;
-    NSString*           runLoopMode;
-    NSTimer*            timer;
-    NSInteger           idleTimer;
-    MQTTEncoder*        encoder;
-    MQTTDecoder*        decoder;
-    NSMutableArray*     queue;
-    UInt16              msgId;
-    id                  delegate;
+    MQTTSessionStatus    status;
+    NSString*            clientId;
+    NSString*            userName;
+    NSString*            password;
+    UInt16               keepAliveInterval;
+    BOOL                 cleanSessionFlag;
+    NSRunLoop*           runLoop;
+    NSString*            runLoopMode;
+    NSTimer*             timer;
+    NSInteger            idleTimer;
+    MQTTEncoder*         encoder;
+    MQTTDecoder*         decoder;
+    NSMutableArray*      queue;
+    UInt16               txMsgId;
+    id                   delegate;
+    NSMutableDictionary* txFlows;
+    NSMutableDictionary* rxFlows;
+    NSMutableArray*      timerRing;
+    unsigned int         ticks;
 }
 
 - (id)initWithClientId:(NSString*)theClientId;
@@ -63,8 +67,15 @@ typedef enum {
 - (void)setDelegate:aDelegate;
 - (void)connectToHost:(NSString*)ip port:(UInt32)port;
 - (void)subscribeTopic:(NSString*)theTopic;
+- (void)subscribeToTopic:(NSString*)topic atLevel:(UInt8)qosLevel;
 - (void)unsubscribeTopic:(NSString*)theTopic;
 - (void)publishData:(NSData*)theData onTopic:(NSString*)theTopic;
+- (void)publishDataAtLeastOnce:(NSData*)theData onTopic:(NSString*)theTopic;
+- (void)publishDataAtLeastOnce:(NSData*)theData onTopic:(NSString*)theTopic retain:(BOOL)retainFlag;
+- (void)publishDataAtMostOnce:(NSData*)theData onTopic:(NSString*)theTopic;
+- (void)publishDataAtMostOnce:(NSData*)theData onTopic:(NSString*)theTopic retain:(BOOL)retainFlag;
+- (void)publishDataExactlyOnce:(NSData*)theData onTopic:(NSString*)theTopic;
+- (void)publishDataExactlyOnce:(NSData*)theData onTopic:(NSString*)theTopic retain:(BOOL)retainFlag;
 - (void)publishJson:(id)payload onTopic:(NSString*)theTopic;
 - (void)timerHandler:(NSTimer*)theTimer;
 - (void)encoder:(MQTTEncoder*)sender handleEvent:(MQTTEncoderEvent) eventCode;
@@ -74,6 +85,13 @@ typedef enum {
 // private methods
 - (void)newMessage:(MQTTMessage*)msg;
 - (void)error:(MQTTSessionEvent)event;
+- (void)handlePublish:(MQTTMessage*)msg;
+- (void)handlePuback:(MQTTMessage*)msg;
+- (void)handlePubrec:(MQTTMessage*)msg;
+- (void)handlePubrel:(MQTTMessage*)msg;
+- (void)handlePubcomp:(MQTTMessage*)msg;
+- (void)send:(MQTTMessage*)msg;
+- (UInt16)nextMsgId;
 
 @end
 
